7130c14fe16cbdf36b8e6416fc50d59f830e7d27
Vadim Belyaev <65543343+ilberic@users.noreply.github.com>
2020-09-24 17:45:49 +0500

    Add files via upload

---
+++ precache-manifest.c349d5dcf5e0ef748af43891d2296f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0292e64b92240716535cfa277b0b48b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb134435593bcc91318",
+    "url": "/static/css/main.5f1fe38c.chunk.css"
+  },
+  {
+    "revision": "878b4604ae5aa261514c",
+    "url": "/static/js/2.cb2dac08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cb2dac08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb134435593bcc91318",
+    "url": "/static/js/main.11daffb5.chunk.js"
+  },
+  {
+    "revision": "8114d0eab9e0b62b6691",
+    "url": "/static/js/runtime-main.bbd9d7f3.js"
+  }
+]);
